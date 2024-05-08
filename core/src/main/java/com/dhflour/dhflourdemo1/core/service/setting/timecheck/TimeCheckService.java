2a6b78cdcca53a704947f6775b2a2cc8219794ff
chonglye chang <chonglye@aartkorea.com>
2024-05-08 18:34:15 +0900

    시간 체크

--- core/src/main/java/com/dhflour/dhflourdemo1/core/service/setting/timecheck/TimeCheckService.java
+++ core/src/main/java/com/dhflour/dhflourdemo1/core/service/setting/timecheck/TimeCheckService.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
@@ -17,14 +21,32 @@ public class TimeCheckService {
     private JdbcTemplate jdbcTemplate;
 
     public TimeInfo getTimeInfo() {
+        TimeInfo timeInfo = new TimeInfo();
         ZonedDateTime serverTime = ZonedDateTime.now();
-        String serverTimeZone = serverTime.getZone().toString();
+        timeInfo.setServerTime(serverTime.toLocalDateTime());
+        timeInfo.setServerTimeZone(serverTime.getZone().toString());
 
-        // Only Mysql
-        LocalDateTime dbLocalDateTime = jdbcTemplate.queryForObject("SELECT now()", LocalDateTime.class);
-        String dbTimeZone = jdbcTemplate.queryForObject("SELECT @@time_zone", String.class);
-        String dbSystemTimeZone = jdbcTemplate.queryForObject("SELECT @@system_time_zone", String.class);
+        DataSource dataSource = jdbcTemplate.getDataSource();
+        if (dataSource != null) {
+            try (Connection connection = dataSource.getConnection()) {
+                DatabaseMetaData metaData = connection.getMetaData();
+                log.debug("Database Product Name: " + metaData.getDatabaseProductName());
+                log.debug("Database Product Version: " + metaData.getDatabaseProductVersion());
+                log.debug("JDBC Driver Name: " + metaData.getDriverName());
+                log.debug("JDBC Driver Version: " + metaData.getDriverVersion());
+                if (metaData.getDatabaseProductName() != null && metaData.getDatabaseProductName().equals("MySQL")) {
+                    LocalDateTime dbLocalDateTime = jdbcTemplate.queryForObject("SELECT now()", LocalDateTime.class);
+                    timeInfo.setDbTime(dbLocalDateTime);
+                    String dbTimeZone = jdbcTemplate.queryForObject("SELECT @@time_zone", String.class);
+                    timeInfo.setDbTimeZone(dbTimeZone);
+                    String dbSystemTimeZone = jdbcTemplate.queryForObject("SELECT @@system_time_zone", String.class);
+                    timeInfo.setDbSystemTimeZone(dbSystemTimeZone);
+                }
+            } catch (SQLException e) {
+                System.err.println("Error getting database metadata: " + e.getMessage());
+            }
+        }
 
-        return new TimeInfo(serverTime.toLocalDateTime(), serverTimeZone, dbLocalDateTime, dbTimeZone, dbSystemTimeZone);
+        return timeInfo;
     }
 }
--- core/src/main/java/com/dhflour/dhflourdemo1/core/types/process/TimeInfo.java
+++ core/src/main/java/com/dhflour/dhflourdemo1/core/types/process/TimeInfo.java
@@ -2,12 +2,14 @@ package com.dhflour.dhflourdemo1.core.types.process;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TimeInfo {
     private LocalDateTime serverTime;
     private String serverTimeZone;
