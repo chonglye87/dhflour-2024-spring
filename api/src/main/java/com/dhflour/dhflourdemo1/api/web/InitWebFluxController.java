076a2e1246159529ff864b4ec6a4f24c3492da12
chonglye chang <chonglye@aartkorea.com>
2024-06-10 14:07:24 +0900

    https://spring.io/blog/2022/10/12/observability-with-spring-boot-3
    설정대기중

--- api/src/main/java/com/dhflour/dhflourdemo1/api/web/InitWebFluxController.java
+++ api/src/main/java/com/dhflour/dhflourdemo1/api/web/InitWebFluxController.java
@@ -1,18 +1,24 @@
 package com.dhflour.dhflourdemo1.api.web;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+// https://spring.io/blog/2022/10/12/observability-with-spring-boot-3
+@Slf4j
 @RestController
 public class InitWebFluxController {
 
-    @Value("${logging.file.name}")
-    private String url;
 
     @GetMapping("/")
-    public Mono<String> helloWorld() {
-        return Mono.just(url);
+    public Mono<String> example() {
+        try {
+            // Your application logic here
+            log.debug("Hello, OpenTelemetry");
+            return Mono.just("Hello, OpenTelemetry!");
+        } catch (Exception e) {
+            throw e;
+        }
     }
 }
